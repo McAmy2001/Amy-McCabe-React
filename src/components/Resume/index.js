dbd191f8525d27b066193c8573be3dcf314ce1d9
Amy McCabe <mcca0168@umn.edu>
2022-09-12 20:47:25 -0500

    resume component added

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,17 @@ import React from 'react';
 function Resume() {
   return(
   <div id='resume'>
-    Resume Section
+    <h1>Resume</h1>
+    <a href={require('../../assets/amymccabe-resume.pdf')}download >Download my resume here.</a>
+    <ul className='skill-list'>Skills:</ul>
+    <li className='skill-list-item'>Javascript</li>
+    <li className='skill-list-item'>MERN Stack</li>
+    <li className='skill-list-item'>MySQL</li>
+    <li className='skill-list-item'>HTML</li>
+    <li className='skill-list-item'>CSS</li>
+    <li className='skill-list-item'>GraphQL</li>
+    <li className='skill-list-item'>TDD</li>
+    <li className='skill-list-item'>OOP</li>
   </div>
 )}
 
