93c5397d376363a04e45738035fb2b9dbea2bee9
Amy McCabe <mcca0168@umn.edu>
2023-01-05 15:03:59 -0600

    Portfolio edits

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,59 +6,64 @@ function Project() {
     {
       title: 'Where Were You When',
       github: 'https://github.com/McAmy2001/Where-Were-You-When',
-      deployed: 'https://where-were-you-when.herokuapp.com/',
+      demo: 'https://drive.google.com/file/d/1C6EWA8fwwzReNiAU_IeZ_Cdg-EA-BYcy/view?usp=sharing',
       alt: 'Screenshot from Where Were You When app',
+      description: 'An app for saving and sharing memories',
       key: 'where'
     },
     {
+      title: 'Social Network API',
+      github: 'https://github.com/McAmy2001/social-network-API',
+      demo: 'https://drive.google.com/file/d/1FnuIkFQh_85KhVpQ8BcnsmWC13FA6h_y/view?usp=sharing',
+      alt: 'Screenshot from app',
+      description: 'A social network API backend',
+      key: 'social'
+    },
+    {
+      title: 'Note Taker',
+      github: 'https://github.com/McAmy2001/Note-Taker',
+      demo: 'https://drive.google.com/file/d/1GcbPPLbGfnvWzIbB0tE7NJOfwAQefcRG/view?usp=sharing',
+      alt: 'Screenshot from app',
+      description: 'A simple note taking app',
+      key: 'note'
+    },
+    {
+      title: 'Team Profile Generator',
+      github: 'https://github.com/McAmy2001/Team-Profile-Generator',
+      demo: 'https://drive.google.com/file/d/15azTnuvrG5-n4NCZOghTncArcsGqRmZX/view?usp=sharing',
+      alt: 'Screenshot from app',
+      description: 'A command line app that generates a team portfolio',
+      key: 'team'
+    },
+    {
+      title: 'Employee Tracker',
+      github: 'https://github.com/McAmy2001/Employee-Tracker',
+      demo: 'https://drive.google.com/file/d/1ov36Aefa2VhTnuU6X6BHd4s9-JGG2BYk/view?usp=sharing',
+      alt: 'Screenshot from app',
+      description: 'A command line app that displays requested employee information from a database in tables',
+      key: 'tracker'
+    }
+    /*{
       title: 'Take A Hike',
       github: 'https://github.com/McAmy2001/Take-A-Hike-2',
-      deployed: 'https://mcamy2001-take-a-hike.herokuapp.com/',
+      demo: 'https://mcamy2001-take-a-hike.herokuapp.com/',
       alt: 'Take A Hike Logo',
       key: 'hike'
     },
     {
       title: 'Get Facts and Find Cats',
       github: 'https://github.com/McAmy2001/Get-Facts-and-Find-Cats',
-      deployed: 'https://mcamy2001.github.io/Get-Facts-and-Find-Cats/',
+      demo: 'https://mcamy2001.github.io/Get-Facts-and-Find-Cats/',
       alt: 'Monster the cat, meowing at the camera',
       key: 'cats'
-    },
-    {
-      title: 'Social Network API',
-      github: 'https://github.com/McAmy2001/social-network-API',
-      deployed: 'https://drive.google.com/file/d/1PglojSptrJ6UPB-Rc7vr6rkAl69Wav7C/view',
-      alt: 'Screenshot from app',
-      key: 'social'
-    },
-    {
+    }, */
+    /*{
       title: 'Tech Blog',
       github: 'https://github.com/McAmy2001/Tech-Blog',
-      deployed: 'https://mcamy-tech-blog.herokuapp.com/',
+      demo: 'https://mcamy-tech-blog.herokuapp.com/',
       alt: 'Screenshot from app',
       key: 'blog'
-    },
-    {
-      title: 'Note Taker',
-      github: 'https://github.com/McAmy2001/Note-Taker',
-      deployed: 'https://shrouded-hamlet-63560.herokuapp.com/',
-      alt: 'Screenshot from app',
-      key: 'note'
-    },
-    {
-      title: 'Team Profile Generator',
-      github: 'https://github.com/McAmy2001/Team-Profile-Generator',
-      deployed: 'https://drive.google.com/file/d/1t6GYVjfg9aCq8yMyezeVdvsPPCCuWa08/view',
-      alt: 'Screenshot from app',
-      key: 'team'
-    },
-    {
-      title: 'Employee Tracker',
-      github: 'https://github.com/McAmy2001/Employee-Tracker',
-      deployed: 'https://drive.google.com/file/d/1eY53Ts_SpbI8EfKOuU5_KY3YrN5s2ONN/view',
-      alt: 'Screenshot from app',
-      key: 'tracker'
-    }
+    }, */
   ]
   return (
     <div className='portfolio-container'>
@@ -66,10 +71,16 @@ function Project() {
       <div className='portfolio'>
         {projects.map((project, i) => (
           <div className='project' key={project.key}>
+            <a href={project.github} target="_blank" rel="noopener noreferrer">
             <img src={require(`../../assets/img${i}.jpeg`)} alt={project.alt} />
             <h3>
-            <a href={project.github}>{project.title}</a><br />
-            </h3><br/>
+            {project.title}<br />
+            </h3>
+            </a><br/>
+            <p>{project.description}</p><br/>
+            <h4>
+              <a href={project.demo} target="_blank" rel="noopener noreferrer">Watch a demo</a>
+            </h4>
           </div>
 
           //key={project.key}
